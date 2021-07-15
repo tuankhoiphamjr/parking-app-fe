e32a68c69e456ff510f7528a15054e69acdb7c23
tuankhoiphamjr <khoi.phambkit_5ting@hcmut.edu.vn>
2021-07-15 17:17:38 +0700

    edit url

--- src/api/BaseURL.js
+++ src/api/BaseURL.js
@@ -1,1 +1,2 @@
-export const BaseURL = 'http://192.168.19.105:8080/api'
+// export const BaseURL = 'http://192.168.19.105:8080/api'
+export const BaseURL = 'https://parking-app-bk.herokuapp.com/api';
